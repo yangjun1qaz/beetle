55d0f7cd481c9999fe6057cc6ea7d0c1effa0a1c
yangjun1qaz <1094557609@qq.com>
2019-10-22 16:22:10 +0800

    ribbon的使用

--- beetle-api/src/main/java/com/brayden/beetleapi/BeetleApiApplication.java
+++ beetle-api/src/main/java/com/brayden/beetleapi/BeetleApiApplication.java
@@ -2,13 +2,22 @@ package com.brayden.beetleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class BeetleApiApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BeetleApiApplication.class, args);
